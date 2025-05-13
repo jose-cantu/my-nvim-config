fc0b2452d5fd78ebeaecf190ab5a1beb654c3fd9
Jason <jason@SilverMagnus.localdomain>
2025-05-12 19:37:12 -0500

    feat: added which-key help menu, flash, lsp highlight fixes

--- init.lua
+++ init.lua
@@ -1,51 +1,66 @@
 --------------------------------------------------------------------------------
--- init.lua (Fresh Minimal Config)
+-- init.lua (Fresh Minimal Config – corrected)
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
--- 1) BOOTSTRAP lazy.nvim
+-- 1) BOOTSTRAP lazy.nvim -------------------------------------------------------
 --------------------------------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable", -- latest stable release
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 --------------------------------------------------------------------------------
--- 2) SETUP Plugins via lazy.nvim
+-- 2) EARLY OPTIONS & GLOBALS ---------------------------------------------------
 --------------------------------------------------------------------------------
--- Puting these at near top of init.lua file (before calling lazy.setup())
-vim.g.loaded_netrw = 1 
-vim.g.loaded_netrwPlugin = 1 
+-- Disable netrw so nvim-tree can own file‑explorer duties
+vim.g.loaded_netrw       = 1
 
+-- Point Neovim’s Python host to the MicroSeq conda env
+vim.g.python3_host_prog = "/home/jason/anaconda3/envs/MicroSeq/bin/python"
+
+--------------------------------------------------------------------------------
+-- 3) LSP HELPER ----------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Highlight all references to symbol under cursor while you pause
+local function lsp_highlight_on_attach(client, bufnr)
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end 
+end
+
+
+
+--------------------------------------------------------------------------------
+-- 4) PLUGINS VIA lazy.nvim -----------------------------------------------------
+--------------------------------------------------------------------------------
 require("lazy").setup({
 
-  -- == THEME: Gruvbox ==
+  {
+    "folke/which-key.nvim",
+    priority = 900,
+    config   = function() require("which-key").setup({}) end,
+  },
+
+-- == THEME: Gruvbox ==
   {
     "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    priority = 1000, -- Load this first so colors apply correctly
+    name     = "gruvbox",
+    priority = 1000, -- Load first so colors apply correctly
     config = function()
-      require("gruvbox").setup({
-        undercurl = true,
-        underline = true,
-        bold = true,
-        contrast = "soft", -- "hard" or "" also possible
-
-        -- If you want coffee/golden overrides, uncomment & tweak:
-        -- palette_overrides = {
-        --   bright_green  = "#a5895b",
-        --   bright_yellow = "#d9bc8c",
-        -- },
-      })
-      vim.cmd("colorscheme gruvbox")
+      require("gruvbox").setup({ contrast = "soft", bold = true })
+      vim.cmd.colorscheme("gruvbox")
     end,
   },
 
@@ -56,21 +71,15 @@ require("lazy").setup({
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "lua", "vim", "python", "bash", "rust" },
-        highlight = { enable = true },
+        highlight        = { enable = true },
       })
     end,
   },
 
-  -- == MASON (Install External LSP/Formatters) ==
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- recommended if Mason is new
-    config = function()
-      require("mason").setup()
-    end,
-  },
+  -- == MASON (binary manager) ==
+  { "williamboman/mason.nvim", build = ":MasonUpdate", config = true },
 
-  -- == MASON-LSPCONFIG (Bridges Mason & lspconfig) ==
+  -- == MASON‑LSPCONFIG (bridge) ==
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "neovim/nvim-lspconfig" },
@@ -79,50 +88,56 @@ require("lazy").setup({
         ensure_installed = { "pyright", "bashls", "rust_analyzer" },
       })
 
-      -- Setup each server automatically
       require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
+        -- default for every server
+        function(server)
+          require("lspconfig")[server].setup({ on_attach = lsp_highlight_on_attach })
         end,
 
-        -- Example: special config for Rust
+        -- Pyright override
+        ["pyright"] = function()
+          require("lspconfig").pyright.setup({
+            on_attach  = lsp_highlight_on_attach,
+            pythonPath = vim.g.python3_host_prog,
+            settings   = {
+              python = {
+                analysis = {
+                  extraPaths         = { vim.fn.getcwd() .. "/src" },
+                  autoSearchPaths    = true,
+                  useLibraryCodeForTypes = true,
+                },
+              },
+            },
+          })
+        end,
+
+        -- Rust analyzer override
         ["rust_analyzer"] = function()
-          require("lspconfig").rust_analyzer.setup({})
+          require("lspconfig").rust_analyzer.setup({ on_attach = lsp_highlight_on_attach })
         end,
       })
     end,
   },
 
-  -- == COMPLETION (nvim-cmp + sources + snippets) ==
+  -- == COMPLETION (nvim‑cmp + sources) ==
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",      -- LSP source
-      "hrsh7th/cmp-path",          -- file paths
-      "hrsh7th/cmp-buffer",        -- current buffer words
-      "L3MON4D3/LuaSnip",          -- snippet engine
-      "saadparwaiz1/cmp_luasnip",  -- snippet completions
+      "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip",
     },
     config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
+      local cmp, luasnip = require("cmp"), require("luasnip")
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body) -- snippet expansion
-          end,
-        },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "buffer" },
-        }),
+        snippet = { expand = function(a) luasnip.lsp_expand(a.body) end },
         mapping = {
-          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<Tab>"]   = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"]    = cmp.mapping.confirm({ select = true }),
+        },
+        sources = {
+          { name = "nvim_lsp" }, { name = "luasnip" },
+          { name = "path" },      { name = "buffer" },
         },
       })
     end,
@@ -131,79 +146,82 @@ require("lazy").setup({
   -- == RUST TOOLS (Optional) ==
   {
     "simrat39/rust-tools.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
     ft = { "rust" },
+    dependencies = "neovim/nvim-lspconfig",
     config = function()
-      local rt = require("rust-tools")
-      rt.setup({
-        server = {
-          on_attach = function(_, bufnr)
-            -- put rust-specific keybinds here if desired
-          end,
-        },
-      })
+      require("rust-tools").setup({ server = { on_attach = lsp_highlight_on_attach } })
     end,
   },
 
   -- == TELESCOPE (Fuzzy Finder) ==
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          -- see :help telescope.setup
-        },
-      })
-    end,
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function() require("telescope").setup({}) end,
   },
 
-  -- == nvim-tree.lua setup
+  -- == FLASH (quick jump / better search) ==
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts  = {},
+    keys  = {
+      { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash jump" },
+    },
+  },
+
+  -- == VIM‑ILLUMINATE (highlight references) ==
+  { "RRethy/vim-illuminate", event = "BufReadPost", opts = { delay = 120 } },
+
+  -- == COMMENT.NVIM (toggle comments) ==
+  { "numToStr/Comment.nvim", keys = { { "gc", mode = { "n", "x" } } }, config = true },
+
+  -- == nvim-tree.lua (file explorer) ==
   {
     "nvim-tree/nvim-tree.lua",
-    -- OPTIONAL: for file icons
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", 
-    },
+    dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
-      -- See full list of options at: 
-      -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
-
       require("nvim-tree").setup({
-        -- BEGIN default options
-        sort_by = "case_sensitive",
-        view = {
-          width = 30,
-          side = "left",
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = false,
-        },
-        -- Here I add onn the on_attach function:
-	on_attach = function(bufnr)
-		local api = require("nvim-tree.api")
-		-- Setup all of nvim-tree's default mappings 
-		api.config.mappings.default_on_attach(bufnr)
-
-		-- Force a custom mapping for "?"
-		vim.keymap.set("n", "?", api.tree.toggle_help, {
-			buffer = bufnr,
-			desc   = "NvimTree Help",
+        sort_by  = "case_sensitive",
+        view     = { width = 30, side = "left" },
+        renderer = { group_empty = true },
+        filters  = { dotfiles = false },
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+          api.config.mappings.default_on_attach(bufnr)
+          vim.keymap.set("n", "?", api.tree.toggle_help, { buffer = bufnr, desc = "NvimTree Help" })
+        end,
       })
     end,
-  })
-  end 
   },
-}) -- end of require("lazy").setup()
+})
 
 --------------------------------------------------------------------------------
--- 3) OPTIONAL NEOVIM SETTINGS
+-- 5) GLOBAL KEYMAPS & MISC OPTIONS -------------------------------------------
 --------------------------------------------------------------------------------
-vim.opt.number = true             -- line numbers
-vim.opt.mouse = "a"               -- enable mouse
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = "Toggle NvimTree" })
+vim.opt.number    = true
+vim.opt.mouse     = "a"
+vim.opt.clipboard = "unnamedplus"
 
+-- Toggle file explorer
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
+-- VS‑Code style comment toggles via Comment.nvim
+vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+vim.keymap.set("v", "<leader>/", "gc",  { remap = true, desc = "Toggle comment block" })
+
+-- 6) HELP SYSTEM (which-key integration) -------------------------------------
+--------------------------------------------------------------------------------
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>"] = {
+    -- Help section grouped under "h"
+    h = {
+      name = "+help",  -- Shows "h +help" as a sub-menu
+      h = { "<cmd>Telescope help_tags<CR>", "Search :help (Telescope)" },  -- Open Telescope help tags
+      t = { "<cmd>WhichKey<CR>", "Show which-key pop-up" },  -- Show keybinding cheat sheet
+      k = { "<cmd>NvimTreeToggle<CR>", "Toggle NvimTree (File Explorer)" },  -- Example: Toggle file explorer
+    },
+  },
+})
