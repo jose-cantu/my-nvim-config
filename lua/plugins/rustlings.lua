4df7b1d776eb0cd82ea99c448df577fae0fe633d
Jason <jason@SilverMagnus.localdomain>
2025-06-30 17:03:04 -0500

    Expanded the nvim-cmp configuration to include buffer and path completion sources, load friendly snippets, and support ghost text and Tab/Shift‑Tab cycling for completion items. LSP servers keep using cmp’s advertised capabilities to enable language-aware completions across the board

--- lua/plugins/rustlings.lua
+++ lua/plugins/rustlings.lua
@@ -29,18 +29,59 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
-      local cmp, ls = require("cmp"), require("luasnip")
+      local cmp = require("cmp")
+      local ls  = require("luasnip")
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
       cmp.setup({
-        snippet = { expand = function(a) ls.lsp_expand(a.body) end },
+        snippet = {
+          expand = function(args) ls.lsp_expand(args.body) end,
+        },
+
         mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif ls.expand_or_jumpable() then
+              ls.expand_or_jump()
+            elseif vim.fn.col(".") > 1 then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif ls.jumpable(-1) then
+              ls.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
+
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip"   },
+          { name = "buffer"    },
+          { name = "path"      },
+        }),
+
+        completion   = { completeopt = "menu,menuone,noinsert" },
+        experimental = { ghost_text = true },
       })
     end,
   },
